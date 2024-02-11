b757c1c6f842562b074ac392a2fb3d0e60a19638
Shayan Beiraghdar <shayanbeiraghdar@gmail.com>
2024-02-11 12:16:44 +0330

    Add front-end files

---
+++ front-end/src/store.js
@@ -0,0 +1,33 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import {
+  productListReducer,
+  productDetailReducer,
+} from "./reducer/productReducer";
+
+import { cartReducer } from "./reducer/cartReducer";
+
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetail: productDetailReducer,
+  cart: cartReducer,
+});
+
+const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromLocalStorage },
+};
+
+const middleWare = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  applyMiddleware(...middleWare)
+);
+
+export default store;
